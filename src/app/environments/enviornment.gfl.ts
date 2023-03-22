1aad0da46579858c3d9a0e552ababe5adb40dc69
tarang rathod <72619617+tarangrathod@users.noreply.github.com>
2023-03-22 12:36:34 +0530

    added enviornment files restored prev code V=1.5.23 ENV=prod

---
+++ src/app/environments/enviornment.gfl.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: true,
+  siteKey: '6LdCGAoaAAAAAEeAuuDDwJL129QScM13a0cmeJ91',
+  // 6LfOMwoaAAAAAAaNM08WwXsxdsFP8-vkQ1nH0dqC
+  showAppList: ["RP", "BM", "AM", "TD", "DS"],
+  tomtomKey: 'a4yfznzr0WueH8kQ27ASjNmRF1GbNfPt',
+  isTomtom: false,
+  wijmoKey: 'gfl.zignexlogistics.com,531633231395724#B0JpMIyNHZisnOiwmbBJye0ICRiwiI34TU9xmRFtUcMhTeTt6SzJTUmdGVZlnaTpUaxZWRsxGS5siZvRmcvp4QtpXS5MmMFx6VTZXRzkUZJZWSF5kSax6NMNUUMlGZ7J6KxVXcVljUDBlaZZ5Y45EdXpGRTl7R7N7MJB7R9R6NTVDTWFFUoNHZVFmeRdjcQ3CaTRzVUxmZLhmbihDVupEaxl4T8NGZLBncINlMiJ6Mxw4Q4Y6YVRWNG5kYh94KvUncaNmQDhzQjZXQBJzMRVXQq9UeWN5TJRlZRVXQ9JkdzEHUHZlZTVmQ7FGUrF4TMFjWIJ7Z4UmYZFnRSpFcTlGcutUav2WTTl5VExGR0V5bCZTT5d4dzE7cIBzNQRlWwEUNOF4bvhVNrVGcqR7Yi3iYhtSbDhnRNJDc5FFMzpHTMh6bB5ma9l5NvAnYuNHMvdjUrZXcFhldlhVerFnU8k4Mx3WSaRWNYp6a4NXcJZmI0IyUiwiI7UUOBZjN6cjI0ICSiwCMzEjN8ITNzUTM0IicfJye#4Xfd5nIJBjMSJiOiMkIsIibvl6cuVGd8VEIgQXZlh6U8VGbGBybtpWaXJiOi8kI1xSfiUTSOFlI0IyQiwiIu3Waz9WZ4hXRgAicldXZpZFdy3GclJFIv5mapdlI0IiTisHL3JyS7gDSiojIDJCLi86bpNnblRHeFBCI73mUpRHb55EIv5mapdlI0IiTisHL3JCNGZDRiojIDJCLi86bpNnblRHeFBCIQFETPBCIv5mapdlI0IiTisHL3JyMDBjQiojIDJCLiUmcvNEIv5mapdlI0IiTisHL3JSV8cTQiojIDJCLi86bpNnblRHeFBCI4JXYoNEbhl6YuFmbpZEIv5mapdlI0IiTis7W0ICZyBlIsISNwYTM6ADIzATMwMjMwIjI0ICdyNkIsISbvNmLzNWa4NXan3Gb8VmbnlmeuwmZnJiOiMXbEJCLiMmbJBCL8VkbnlmWiojIh94QiwiI4IzN5kzMxMjMzMjNxMTNiojIklkIs4XXbpjInxmZiwiIzYXMyAjMiojIyVmdiwSZwxZY',
+  rpApiUrl: "https://api.gfl.zignexlogistics.com/zexrp",
+  apiUrl: 'https://api.gfl.zignexlogistics.com/zextd/',
+  authenticationEnabled: true,
+  geoServerUrl: 'https://geoserver.gfl.zignexlogistics.com/geoserver/zignex/wms',
+  mapTileGeoserverUrl: 'https://geoserver.gfl.zignexlogistics.com/geoserver/wms',
+  appUrl: 'https://gfl.zignexlogistics.com/zexbm/',
+  keycloak: {
+    // Url of the Identity Provider
+    issuer: 'https://sso.gfl.zignexlogistics.com/auth/',
+    // Realm
+    realm: 'zx-sso',
+    // The SPA's id.
+    // The SPA is registerd with this id at the auth-serverß
+    clientId: 'zignex-boundary-mgmt-ui',
+  },
+};
